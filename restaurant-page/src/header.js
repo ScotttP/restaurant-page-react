a658a4c088eede66fe7da2f9ae3b778ae545eb7d
Scott Pribe <Scottpribe@gmail.com>
2020-07-26 18:07:29 -0400

    initial commit

---
+++ restaurant-page/src/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+class Header extends React.Component {
+    render () {
+        return (
+            <div>
+                <h1>This is My header</h1>
+                <h2>This is another one</h2>
+            </div>
+            
+        )
+    }
+       
+}
+
+export default Header
