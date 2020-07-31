e67c1d9fa47cbfdede1dea8bc8dc2d5510054ecb
Scott Pribe <Scottpribe@gmail.com>
2020-07-31 10:13:23 -0400

    Updates

---
+++ precache-manifest.3d66046aea35ee2dad196629b0f464b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bc985e444b57177dcb3470212ffa422",
+    "url": "/restaurant-page-react/index.html"
+  },
+  {
+    "revision": "f607f3039bc8b4a820b1",
+    "url": "/restaurant-page-react/static/css/main.f6138149.chunk.css"
+  },
+  {
+    "revision": "31b11ed726fc7bf03b0f",
+    "url": "/restaurant-page-react/static/js/2.81d047cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/restaurant-page-react/static/js/2.81d047cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f607f3039bc8b4a820b1",
+    "url": "/restaurant-page-react/static/js/main.a50cdbee.chunk.js"
+  },
+  {
+    "revision": "1a3fded9d9eb3132bf33",
+    "url": "/restaurant-page-react/static/js/runtime-main.53c7abe8.js"
+  }
+]);
